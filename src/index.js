8e51ee0c713b83293adab81d3594cb5fd22f8bc1
Nicolas Dieudonne <nicolas.dieudonne@clearstream.com>
2020-09-25 08:45:07 +0000

    fix

--- src/index.js
+++ src/index.js
@@ -1,36 +1,32 @@
 import React, { Component, Fragment } from "react";
-import { cloneDeep } from "lodash";
 import "./index.css";
-import Board from "./components/Board"
+import Board from "./components/Board";
 
 class BoardLib extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
-
-const {
-  groups=[],
-  locked=true,
-  updatedDatas=null,
-  deletable= false,
-  columnsWidth= 250,
-  cardsHeight= 50,
-  columnsAction=null
-} = this.props;
+    const {
+      groups = [],
+      locked = true,
+      updatedDatas = null,
+      columnsWidth = 250,
+      cardsHeight = 50,
+      columnsAction = null
+    } = this.props;
 
     return (
       <>
         <Board
-        className="o2xp-react-kanban"
+          className="o2xp-react-kanban"
           groups={groups}
           locked={locked}
           updatedDatas={updatedDatas}
           columnsWidth={columnsWidth}
           cardsHeight={cardsHeight}
           columnsAction={columnsAction}
-
         />
       </>
     );
